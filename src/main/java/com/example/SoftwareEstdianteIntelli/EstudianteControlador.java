d2c21fc904e14ede4635ebfe604d1028c83fca05
Celis <mau.armata@gmail.com>
2019-09-14 19:20:06 -0400

    Adicionando la dependencia hateoas y los nuevos metodos de servicios Restfull en operaciones GET de clase Estudiante controlador

--- src/main/java/com/example/SoftwareEstdianteIntelli/EstudianteControlador.java
+++ src/main/java/com/example/SoftwareEstdianteIntelli/EstudianteControlador.java
@@ -1,8 +1,14 @@
 package com.example.SoftwareEstdianteIntelli;
 
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.core.DummyInvocationUtils.methodOn;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 @RestController
 class EstudianteControlador  {
@@ -11,21 +17,43 @@ class EstudianteControlador  {
     public EstudianteControlador(EstudianteRepositorio repositorio) {
         this.repositorio = repositorio;
     }
-
+/*
     @GetMapping("/estudiantes")
     List<Estudiante> todo(){
         return  repositorio.findAll();
+    }
+*/
+    @GetMapping("/estudiantes")
+    Resources<Resource<Estudiante>> todo(){
+        List<Resource<Estudiante>> estudiantes=repositorio.findAll().stream()
+                .map(estudiante ->new Resource<>(estudiante,
+                        linkTo(methodOn(EstudianteControlador.class).uno(estudiante.getId())).withSelfRel(),
+                        linkTo(methodOn(EstudianteControlador.class).todo()).withRel("estudiantes")))
+                .collect(Collectors.toList());
+        return  new Resources<>(estudiantes,linkTo(methodOn(EstudianteControlador.class)).withSelfRel());
     }
 
     @PostMapping("/estudiantes")
     Estudiante nuevoEstudiante(@RequestBody Estudiante estudiante){
         return repositorio.save(estudiante);
     }
-
+/*
     @GetMapping("/estudiante/{id}")
     Estudiante uno(@PathVariable Long id){
         return repositorio.findById(id).orElseThrow(()-> new  ExcepcionEstudiantenoEncontrado(id));
     }
+*/
+    @GetMapping("/estudiante/{id}")
+    Resource<Estudiante> uno(@PathVariable Long id){
+        Estudiante estudiante=repositorio.findById(id)
+                .orElseThrow(()-> new ExcepcionEstudiantenoEncontrado(id));
+        return new Resource<>(
+                estudiante,linkTo(methodOn(EstudianteControlador.class).uno(id)).withSelfRel(),
+                linkTo(methodOn(EstudianteControlador.class).todo()).withRel("estudiantes")
+        );
+    }
+
+
 
     @PutMapping("/estudiante/{id}")
     Estudiante reemplazarEstudiante(@RequestBody Estudiante nuevoEstudiante, @PathVariable Long id)
